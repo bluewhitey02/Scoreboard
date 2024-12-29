2ddf175e9b9a332807daeb6ddc60f5122701ca3e
Najoan <68422289+najoan125@users.noreply.github.com>
2024-12-30 05:13:34 +0900

    init

---
+++ src/main/java/kr/hyfata/zero/ZeroScoreboard.java
@@ -0,0 +1,17 @@
+package kr.hyfata.zero;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZeroScoreboard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
